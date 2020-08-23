3ef96b5747f88c6228d3187bb4ba95fcc2bdc534
gonzalo <alejandro@lavacoders.com>
2020-08-22 22:18:38 -0300

    14

--- detail.php
+++ detail.php
@@ -1,7 +1,7 @@
 <?php
 	// SDK de Mercado Pago
 	require __DIR__ .  '/vendor/autoload.php';
-
+die($_SERVER['SERVER_NAME'].substr($_POST['img'],1));
 	// Agrega credenciales
 	MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 	MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
@@ -18,8 +18,8 @@
 	$item->id="1234";
 	$payer = new MercadoPago\Payer();
 	$payer->email = "test_user_63274575@testuser.com";
-	$payer->first_name = "Lalo";
-	$payer->last_name = "Landa";
+	$payer->name = "Lalo";
+	$payer->surname = "Landa";
 	$payer->phone = array(
 		"area_code" => "11",
 		"number" => "22223333"

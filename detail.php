70402bafb85b4fac38412d0565307904c724c604
gonzalo <alejandro@lavacoders.com>
2020-08-22 21:23:04 -0300

    12

--- detail.php
+++ detail.php
@@ -3,7 +3,7 @@
 	require __DIR__ .  '/vendor/autoload.php';
 
 	// Agrega credenciales
-	MercadoPago\SDK::setAccessToken('TEST-7448852130109970-082217-984cc9aee821a0bfeede7b1016044f95-629974774');
+	MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 	MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 	// Crea un objeto de preferencia
 	$preference = new MercadoPago\Preference();
--- notificaciones.php
+++ notificaciones.php
@@ -2,7 +2,7 @@
  
   require_once 'vendor/autoload.php'; // You have to require the library from your Composer vendor folder
 
-  MercadoPago\SDK::setAccessToken("TEST-7448852130109970-082217-984cc9aee821a0bfeede7b1016044f95-629974774"); // Either Production or SandBox AccessToken
+  MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"); // Either Production or SandBox AccessToken
 
   MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
